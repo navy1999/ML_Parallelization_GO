89e470639e916608cc29d70703cbb0e4c67c090b
Navneet Shankar <32503754+navy1999@users.noreply.github.com>
2024-12-11 19:45:57 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,47 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func generateData(numSamples, numFeatures int) []Point {
 	data := make([]Point, numSamples)
 	for i := range data {
-	    data[i].features = make([]float64, numFeatures)
-	    for j:=range data[i].features{
-	        data[i].features[j]=rand.Float64()
-	    }
-	    data[i].label=rand.Intn(2) // Binary classification (label can be either 0 or 1).
-	  }
-	  return data	
+		data[i].features = make([]float64, numFeatures)
+		for j := range data[i].features {
+			data[i].features[j] = rand.Float64()
+		}
+		data[i].label = rand.Intn(2) // Binary classification (0 or 1)
+	}
+	return data
 }
 
 func main() {
-
 	rand.Seed(time.Now().UnixNano())
 
-	numTrainSamples:=10000 
-	numTestSamples:=200 
-	numFeatures:=10 
+	numTrainSamples := 10000
+	numTestSamples := 200
+	numFeatures := 10
 
-   trainData:=generateData(numTrainSamples,numFeatures)
-   testData:=generateData(numTestSamples,numFeatures)
+	trainData := generateData(numTrainSamples, numFeatures)
+	testData := generateData(numTestSamples, numFeatures)
 
-	k:=5 // Number of nearest neighbors
+	k := 5 // Number of nearest neighbors
 
+	// Benchmark KNN
 	fmt.Println("Benchmarking KNN...")
-	durationKNN:=benchmarkKNN(trainData,testData,k)
-	fmt.Printf("Time taken for KNN classification: %v\n",durationKNN)
+	durationKNN := benchmarkKNN(trainData, testData, k)
+	fmt.Printf("Time taken for KNN classification: %v\n", durationKNN)
 
-
+	// Benchmark Random Forest
 	fmt.Println("Benchmarking Random Forest...")
-	durationRF:=benchmarkRandomForest(trainData,testData ,100 ) 
-	fmt.Printf("Time taken for Random Forest classification: %v\n",durationRF)
+	durationRF := benchmarkRandomForest(trainData, testData, 100) // Example parameters
+	fmt.Printf("Time taken for Random Forest classification: %v\n", durationRF)
 
-
+	// Benchmark Neural Network
 	fmt.Println("Benchmarking Neural Network...")
-	durationNN:=benchmarkNeuralNetwork(trainData,testData ,32 ,100 ,0.01 )
-	fmt.Printf("Time taken for Neural Network training and prediction: %v\n",durationNN )
+	durationNN := benchmarkNeuralNetwork(trainData, testData, 32, 100, 0.01) // Example parameters
+	fmt.Printf("Time taken for Neural Network training and prediction: %v\n", durationNN)
+
+	// Benchmark PCA
+	datasetConfigs := [][2]int{{1000, 50}, {5000, 50}, {10000, 50}}
+	threadCounts := []int{1, 2, 4, 8, 16}
+	results := runPCABenchmark(datasetConfigs, threadCounts)
+
+	for _, config := range datasetConfigs {
+		fmt.Printf("PCA Benchmark for Dataset: %dx%d\n", config[0], config[1])
+		fmt.Println("Threads\tTime(s)\tSpeedup")
+		for i, threads := range threadCounts {
+			fmt.Printf("%d\t%.4f\t%.4f\n", threads,
+				results[config]["execution_times"][i],
+				results[config]["speedups"][i])
+		}
+		fmt.Println()
+	}
 }
